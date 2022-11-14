75d3c03a166d3daaf55d0c46ef9e43502bde3aa6
Vinit3211 <vinit.kumar@borngroup.com>
2022-11-14 23:44:20 +0530

    new

--- custom-application-config.mjs
+++ custom-application-config.mjs
@@ -13,7 +13,7 @@ const config = {
     },
     production: {
       applicationId: 'clah28vtp022nuv0159t7w73n',
-      url: 'https://gloabal-new.vercel.app',
+      url: 'https://mc.europe-west1.gcp.commercetools.com.vercel.app',
     },
   },
   oAuthScopes: {
