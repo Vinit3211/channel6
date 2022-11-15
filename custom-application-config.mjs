9b1d72f40a2b27ecaaab7de7d75598696aafafb5
Vinit3211 <vinit.kumar@borngroup.com>
2022-11-16 01:34:05 +0530

    new4

--- custom-application-config.mjs
+++ custom-application-config.mjs
@@ -13,7 +13,7 @@ const config = {
     },
     production: {
       applicationId: 'clah28vtp022nuv0159t7w73n',
-      url: 'https://channels.firebase.app',
+      url: 'https://channels.vercel.app',
     },
   },
   oAuthScopes: {
